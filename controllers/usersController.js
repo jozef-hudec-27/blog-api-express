679f32be670080111eda7a68220ed28deed3fe0c
Jozef Hudec <jojohudec27@gmail.com>
2023-11-18 09:22:39 +0100

    Add route to make user an author

---
+++ controllers/usersController.js
@@ -0,0 +1,28 @@
+const { body, validationResult } = require('express-validator')
+const { protectRoute } = require('../utils/auth')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/user')
+
+exports.enableAuthor = [
+  protectRoute,
+
+  body('code', 'Incorrect code.').trim().equals(process.env.AUTHOR_CODE),
+
+  asyncHandler(async (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Unauthorized.' })
+    } else if (req.user.isAuthor) {
+      return res.status(204).end()
+    }
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    await User.findByIdAndUpdate(req.user._id, { isAuthor: true })
+
+    res.status(200).json({ message: 'You are now an author.' })
+  }),
+]
--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,6 +1,9 @@
 const { protectRoute } = require('../utils/auth')
+const usersController = require('../controllers/usersController')
 
 const router = require('express').Router()
+
+router.post('/users/author', usersController.enableAuthor)
 
 router.get(
   '/protected',
@@ -8,6 +11,7 @@ router.get(
   protectRoute,
 
   (req, res) => {
+    console.log(req.user)
     res.json({ message: 'You are authorized.' })
   }
 )
